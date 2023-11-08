84750c41a2335be9c3c7f3f06e69c9c25e5d15b8
Gustavo Muzeka <muzeka89@gmail.com>
2023-11-08 02:37:24 -0300

    Atualização youtube clone

--- src/hooks/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { MenuProvider } from './Menu';
-
-const AppProvider: React.FC = ({ children }) => (
-  <MenuProvider>{children}</MenuProvider>
-);
-
-export default AppProvider;
